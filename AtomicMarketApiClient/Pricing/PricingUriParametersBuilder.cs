ea0f2740e7054ba82137f5aba9835d05df91e306
Ash Lee <ashleewawa17@gmail.com>
2022-06-16 13:21:13 +0000

    Pricing changes

--- AtomicMarketApiClient/Pricing/PricingUriParametersBuilder.cs
+++ AtomicMarketApiClient/Pricing/PricingUriParametersBuilder.cs
@@ -5,37 +5,79 @@ namespace AtomicMarketApiClient.Pricing
 {
     public class PricingUriParametersBuilder : IUriParameterBuilder
     {
+/* A private variable that is used to store the value of the symbol parameter. */
         private string _symbol;
+/* A nullable boolean specfying burned assets. */
         private bool? _burned;
+/* A private variable that is used to store the value of the collectionName parameter. */
         private string _collectionName;
+/* A private variable that is used to store the value of the schemaName parameter. */
         private string _schemaName;
+/* A private variable that is used to store the value of the templateId parameter. */
         private string _templateId;
+/* A nullable boolean specfying transferable assets. */
         private bool? _isTransferable;
+/* A nullable boolean specfying burnable assets. */
         private bool? _isBurnable;
 
+
+/// <summary>
+/// `WithSymbol` sets the `symbol` parameter
+/// </summary>
+/// <param name="symbol">Token symbols.</param>
+/// <returns>
+/// The PricingUriParameterBuilder object.
+/// </returns>
         public PricingUriParametersBuilder WithSymbol(string symbol)
         {
             _symbol = symbol;
             return this;
         }
-        
+
+
+/// <summary>
+/// `WithBurned` sets the `_burned` field to the value of the `burned` parameter
+/// </summary>
+/// <param name="burned">It filters for burned assets.</param>
+/// <returns>
+/// The PricingUriParameterBuilder object.
+/// </returns>
         public PricingUriParametersBuilder WithBurned(bool burned)
         {
             _burned = burned;
             return this;
         }
 
+
+/// <summary>
+/// `WithCollectionName` is a function that takes a string as a parameter and returns an
+/// `PricingUriParameterBuilder` object
+/// </summary>
+/// <param name="collectionName">The name of the collection you want to query.</param>
+/// <returns>
+/// The PricingUriParameterBuilder object.
+/// </returns>
         public PricingUriParametersBuilder WithCollectionName(string collectionName)
         {
             _collectionName = collectionName;
             return this;
         }
 
+
+/// <summary>
+/// > This function sets the schema name for the query
+/// </summary>
+/// <param name="schemaName">The name of the schema to use.</param>
+/// <returns>
+/// The PricingUriParameterBuilder object.
+/// </returns>
         public PricingUriParametersBuilder WithSchemaName(string schemaName)
         {
             _schemaName = schemaName;
             return this;
         }
+
+
 
         public PricingUriParametersBuilder WithTemplateId(string templateId)
         {
